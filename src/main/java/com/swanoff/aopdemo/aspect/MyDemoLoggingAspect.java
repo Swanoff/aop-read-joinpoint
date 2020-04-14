fc385b7aa05942c7f3ee44dfe97fc5a53a637c13
Surya <suryathegreat.07@gmail.com>
2020-04-14 19:00:52 +0530

    Displaying the method signature using JoinPoint
    JoinPoint has metadata about method call

--- src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,9 @@
 package com.swanoff.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,13 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 	
 	@Before("com.swanoff.aopdemo.aspect.AopExpressions.forDaoPackageNotGetterSetter()")
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 		System.out.println("=====> Executing @Before advice on method");
+		
+		// display the method signature
+		MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSignature);
+		
+		// display method arguements
 	}
 }
