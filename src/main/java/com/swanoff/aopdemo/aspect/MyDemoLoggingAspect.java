eb0f488391cf9967cee15a8a436643d911f14e6e
Surya <suryathegreat.07@gmail.com>
2020-04-14 19:28:10 +0530

    Displaying method arguements with JoinPoints

--- src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import com.swanoff.aopdemo.Account;
+
 @Aspect
 @Component
 @Order(2)
@@ -21,5 +23,21 @@ public class MyDemoLoggingAspect {
 		System.out.println("Method: " + methodSignature);
 		
 		// display method arguements
+		
+		// get args
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop through args
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+			
+			if (tempArg instanceof Account) {
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+				
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());
+			}
+		}
 	}
 }
